7302dbc73a3822a2642642ea3d651d7de929db73
Tim Martin <tim@timmart.in>
2023-05-31 02:59:32 -0400

    my work

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,12 +8,16 @@
 // - An apple costs 2 rustbucks.
 // - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
 // Write a function that calculates the price of an order of apples given
-// the quantity bought. No hints this time!
-
-// I AM NOT DONE
+// the quantity 
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: u16) -> u16 {
+    if apples <= 40 {
+        apples * 2
+    } else {
+        apples
+    }
+}
 
 // Don't modify this function!
 #[test]
